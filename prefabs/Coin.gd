7ff1930600f4d1a3a14b55d1471706a3dba18277
Frank Hale <frankhale@gmail.com>
2021-04-25 13:57:27 -0400

    Change layout of Coin prefab

--- prefabs/Coin.gd
+++ prefabs/Coin.gd
@@ -1,9 +1,8 @@
-extends Node2D
+extends Area2D
 
 export (int) var coin_value = 10
 
 func _on_Area2D_body_entered(body):	
-	var parent_node = get_parent()
-	parent_node.queue_free()	
+	queue_free()	
 	print("player picks up coin")
 	Global.emit_signal("update_player_score", coin_value)
