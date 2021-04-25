dceeb24e5b310ac7e43e8a8f4d86472a3d05b370
Frank Hale <frankhale@gmail.com>
2021-04-25 13:44:40 -0400

    Expose coin value to editor

--- prefabs/Coin.gd
+++ prefabs/Coin.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+export (int) var coin_value = 10
+
 func _on_Area2D_body_entered(body):	
-	self.queue_free()	
+	var parent_node = get_parent()
+	parent_node.queue_free()	
 	print("player picks up coin")
-	Global.emit_signal("update_player_score", 10)
+	Global.emit_signal("update_player_score", coin_value)
